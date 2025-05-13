be0e619ec0f5cf44b25a34354e0240d3c9f88379
Brik000 <35845838+Brik000@users.noreply.github.com>
2025-05-13 11:48:43 -0500

    add

--- account-service/src/main/java/com/devtest/account/config/RabbitMQConfig.java
+++ account-service/src/main/java/com/devtest/account/config/RabbitMQConfig.java
@@ -1,8 +1,12 @@
 package com.devtest.account.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+
 
 @Configuration
 public class RabbitMQConfig {
@@ -15,5 +19,17 @@ public class RabbitMQConfig {
     @Bean
     public Queue respuestaQueue() {
         return new Queue("reporte.respuesta.queue");
+    }
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
     }
 }
--- customer-service/src/main/java/com/devtest/customer/config/RabbitMQConfig.java
+++ customer-service/src/main/java/com/devtest/customer/config/RabbitMQConfig.java
@@ -2,13 +2,14 @@ package com.devtest.customer.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 
 @Configuration
 public class RabbitMQConfig {
-
 
     @Bean
     public Queue solicitudQueue() {
@@ -18,5 +19,18 @@ public class RabbitMQConfig {
     @Bean
     public Queue reporteRespuestaQueue() {
         return new Queue("reporte.respuesta.queue", true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
     }
 }
